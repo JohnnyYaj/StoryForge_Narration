a64446d91f8072a63051ed4464f159f4feeea609
Johnny Yang <125516481+JohnnyYaj@users.noreply.github.com>
2023-09-17 01:13:11 -0500

    Add files via upload
    finished the create video function, which makes a video over some gameplay and saves it to a folder called Output, which will be created if there isn't one

--- Stories.py
+++ Stories.py
@@ -1,6 +1,9 @@
 import openai
 import random
 from google.cloud import texttospeech
+from moviepy.editor import VideoFileClip, vfx ,AudioFileClip, afx, CompositeAudioClip, concatenate_videoclips
+from mutagen.mp3 import MP3
+import os
 
 
 
@@ -8,19 +11,23 @@ from google.cloud import texttospeech
 # Find an API To make convert thr story to a audio file the push it to tiktok or youtube 
 # Maybe make it faster?
 # Find a Video editing API 
+# Create a Function in which creates a txt file of the story 
+# Needs to save the story and write it into a txt file
+
 
 API_keys = open("API_KEY.txt",'r').read()
 openai.api_key = API_keys # OpenAi API Key to access OpenAi chatbase 
 stories_Genre = ["Science Fiction", "Fantasy","Mystery/Thriler","Romance","Historical Fiction","Adventure","Dystopian","Young Adult","Horror","Slice of Life"]
 stories_made = []
+save_output = []
+
+if not os.path.exists("Output1"):
+    os.makedirs("test1")
 
 # Picks 2 Random Genre
 def get_RandomGenre():
-    Genres = random.sample(stories_Genre,2)
-    Genre1 = Genres[0]
-    Genre2 = Genres[1]
-        
-    return "can you create a story involving these 2 genres " + Genre2 + " and "+Genre1 + "and remove the Chapter # and just continue it like usual"
+    genre =  ", ".join(random.sample(stories_Genre,(len(stories_Genre)//2)-1))
+    return "can you create a story involving these genres " + genre + " and remove the Chapter # and just continue it like usual"
 
 # Makes a story genrated from 2 genres
 def make_stories():
@@ -29,27 +36,45 @@ def make_stories():
         model = "gpt-3.5-turbo",
         messages = stories_made
     )
+    # save_textoutput.append(response['choices'][0]['message']['content'])
+    print("Finished creating the story")
     return response['choices'][0]['message']['content']
 
 def synthesize_text(text): # The Main TTS 
     client = texttospeech.TextToSpeechClient.from_service_account_file("key.json") # Google API KEY to access tts 
 
-    synthesis_input = texttospeech.SynthesisInput(text=text)
+    synthesis_input = texttospeech.SynthesisInput(text=text) # Story 
 
-    voice = texttospeech.VoiceSelectionParams(language_code="en-US-Polyglot-1", ssml_gender=texttospeech.SsmlVoiceGender.MALE) #Check this otherwise go back to te regular code 
+    voice = texttospeech.VoiceSelectionParams(language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL) 
     audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
     
     response = client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
     
     rand_num = "".join([str(random.randint(0,5)) for x in range(5)]) # Creates a code so it is different from the others 
     output = f"output{rand_num}.mp3"
+    save_output.append(output)
     
     with open(output, "wb") as out: # Makes the MP3 
         out.write(response.audio_content)  
         print(f'Audio content written to file "{output}"')
 
 
-if __name__ == "__main__":  
-    synthesize_text("Hello World")
+def create_video():
+    name = save_output.pop()
+    backg_audio = int(MP3(name).info.length)+1
+    clip1 = VideoFileClip("minecraft_gameplay.mp4").subclip(0,backg_audio).fx(vfx.fadein,1).fx(vfx.fadeout, 1).fx(vfx.resize,width=1920,height=1080)
+    audio = AudioFileClip(name).fx(afx.audio_fadein, 1)
+    combine = clip1
+    combine.audio = CompositeAudioClip([audio])
+    final_video = f"New_{name.strip('.mp3')}.mp4"
+    save_output.append(final_video )
+    combine.write_videofile(final_video)
+    os.rename(name, f'Output/{name}')
+    os.rename(final_video, f'Output/{final_video}')
+    
 
+
+if __name__ == "__main__":  
+    synthesize_text(make_stories())
+    create_video()
 
